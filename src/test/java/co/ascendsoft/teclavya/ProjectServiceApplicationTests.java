b72a012eeb1e7088ed99d87a11b821f172fd56fd
Srinivas Sai <srinibash.sai26@gmail.com>
2025-07-07 19:41:31 +0530

    project init

---
+++ src/test/java/co/ascendsoft/teclavya/ProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ascendsoft.teclavya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
